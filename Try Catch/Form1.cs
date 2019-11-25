5774b358c675013d13f611df8394b47304d5f963
Steven Macpherson <rc0039062@student.riversidecollege.ac.uk>
2019-11-25 15:41:09 +0000

    Added

--- Try Catch/Form1.cs
+++ Try Catch/Form1.cs
@@ -15,6 +15,19 @@ namespace Try_Catch
         public Form1()
         {
             InitializeComponent();
+            string s = "3";
+            string pi = "3.14159265";
+            int i = int.Parse(s);
+            double d = double.Parse(pi);
+            richTextBox1.AppendText(i.ToString() + "\n");
+            richTextBox1.AppendText(d.ToString() + "\n");
+            string st = "three";
+            try
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
